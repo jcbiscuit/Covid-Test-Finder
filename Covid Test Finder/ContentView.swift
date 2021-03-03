9bdd4e12b25e9e97904d800d986b518093d5d265
Jody Mazinyi <jodyclelland8@gmail.com>
2021-03-03 11:19:01 +1100

    Initial Commit

---
+++ Covid Test Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid Test Finder
+//
+//  Created by Jody Clelland on 3/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
