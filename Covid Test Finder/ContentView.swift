11289dec29e6287be1571d779f3541b9d3bd5bf6
Jody Mazinyi <jodyclelland8@gmail.com>
2021-03-03 12:02:03 +1100

    set up splash screen and app icon

--- Covid Test Finder/ContentView.swift
+++ Covid Test Finder/ContentView.swift
@@ -7,9 +7,48 @@
 
 import SwiftUI
 
+struct SplashScreenView: View {
+    
+    @State var animate = false
+    @State var endSplash = false
+    
+    var body: some View {
+        ZStack {
+            background(Color("SplashScreenBackground"))
+            
+            Image("Logo")
+                .resizable()
+                .renderingMode(.original)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: animate ? nil : 45, height: animate ? nil: 45)
+                .scaleEffect(animate ? 3 : 1)
+                .frame(width: UIScreen.main.bounds.width)
+        }
+        
+        .ignoresSafeArea(.all, edges: .all)
+        .onAppear(perform: animateSplash)
+        .opacity(endSplash ? 0 : 1)
+        
+        
+    }
+    
+    func animateSplash() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            withAnimation(Animation.easeOut(duration: 1.0)) {
+                animate.toggle()
+            }
+        }
+        
+    }
+}
+       
+            
+            
+   
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Find Your Nearest Testing Centre")
             .padding()
     }
 }
